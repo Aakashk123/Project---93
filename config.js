00994a49f839c32737fadbc2da963a161b61e916
Aakashk123 <83151262+Aakashk123@users.noreply.github.com>
2021-09-17 13:16:14 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+ apiKey: "AIzaSyD86-y6bhV3X-q_lPDgWa7Jsb40kSS-BV4",
+  authDomain: "chat-app-a830a.firebaseapp.com",
+  databaseURL: "https://chat-app-a830a-default-rtdb.firebaseio.com",
+  projectId: "chat-app-a830a",
+  storageBucket: "chat-app-a830a.appspot.com",
+  messagingSenderId: "955182725347",
+  appId: "1:955182725347:web:02a3a820eeb9b558a7edd6"
+};
